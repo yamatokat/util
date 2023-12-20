8e24179f27676b01ff770f3b085184d976968bde
Yamato <katsuya.yamato@toyota.global>
2023-12-20 16:39:34 +0900

    add poetry.toml

--- developer/developer/time.py
+++ developer/developer/time.py
@@ -1,11 +1,13 @@
 # current_time_function.py
 from datetime import datetime
 
+
 def get_current_time():
     # 現在の時刻を取得
     current_time = datetime.now()
     # 時刻を文字列に変換して返す
     return current_time.strftime("%Y-%m-%d %H:%M:%S")
+
 
 # ファイルが直接実行された場合にだけ実行されるコード
 if __name__ == "__main__":
